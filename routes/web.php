cf6fc11426cb56ee43340c5528b40e89d52af7d9
Izzatullayev <63715728+Izzatullayev@users.noreply.github.com>
2022-02-23 16:49:27 +0600

    First commit

---
+++ routes/web.php
@@ -0,0 +1,41 @@
+<?php
+
+use App\Http\Controllers\Admin\CategoryController;
+use App\Http\Controllers\Admin\PostController;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+// log in for admin
+
+Route::middleware(['role:admin'])->prefix('adminPanel')->group( function () {
+    Route::get('/', [App\Http\Controllers\Admin\HomeController::class, 'index'])->name('adminPanel');
+
+    Route::resource('category', CategoryController::class);
+
+    Route::resource('post', PostController::class);
+});
+//Route::middleware(['role:admin'])->prefix('admin_panel')->group(function () {
+//    Route::get('/', [App\Http\Controllers\Admin\HomeController::class, 'index'])->name('homeAdmin'); // /admin
+//
+//    Route::resource('category', CategoryController::class);
+//    Route::resource('post', PostController::class);
+//});
