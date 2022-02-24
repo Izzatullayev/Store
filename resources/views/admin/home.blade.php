9a6acd2fd5cd16666825d09114b2939c95ca7271
Cheburek <azat.b8@mail.ru>
2022-02-24 12:42:55 +0600

    Azat's commit

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -6,7 +6,7 @@
             <div class="container-fluid">
                 <div class="row mb-2">
                     <div class="col-sm-6">
-                        <h1 class="m-0">Main page</h1>
+                        <h1 class="m-0">Main page111111</h1>
                     </div>
                 </div>
             </div>
